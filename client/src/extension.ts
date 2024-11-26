3b6883fadeed9631089600c1a4addd5086d482af
CopyTIME <zhou.yuxuan@outlook.com>
2024-11-26 17:23:02 +0800

    Add debugger support.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,10 +1,13 @@
 import * as path from 'path';
-import { ExtensionContext, workspace } from 'vscode';
+import { ExtensionContext, workspace, commands, window, Uri, extensions } from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
-
+let extensionBinFolder = ""
 export function activate(context: ExtensionContext) {
+
+	extensionBinFolder = context.asAbsolutePath(path.join('out','bin'))
+
 	const serverModule = context.asAbsolutePath(
 		path.join('out', 'server.js')
 	);
@@ -31,14 +34,14 @@ export function activate(context: ExtensionContext) {
 	const clientOptions: LanguageClientOptions = {
 		documentSelector: [{ scheme: 'file', language: 'unrealscript' }],
 		synchronize: {
-            configurationSection: 'unrealscript',
+			configurationSection: 'unrealscript',
 			fileEvents: [
-                workspace.createFileSystemWatcher('**/*.{uc,uci}'),
-                // Let's not watch for upk changes, just u files because those are expected to change often.
-                workspace.createFileSystemWatcher('**/*.{u}')
-            ]
+				workspace.createFileSystemWatcher('**/*.{uc,uci}'),
+				// Let's not watch for upk changes, just u files because those are expected to change often.
+				workspace.createFileSystemWatcher('**/*.{u}')
+			]
 		},
-        diagnosticCollectionName: 'UnrealScript',
+		diagnosticCollectionName: 'UnrealScript',
 		outputChannelName: 'UnrealScript',
 	};
 
@@ -49,6 +52,9 @@ export function activate(context: ExtensionContext) {
 		clientOptions
 	);
 	client.start();
+
+	let disposable = commands.registerCommand('extension.unrealscript-debugger-install-interface', installInterface);
+	context.subscriptions.push(disposable);
 }
 
 export function deactivate(): Thenable<void> | undefined {
@@ -56,4 +62,51 @@ export function deactivate(): Thenable<void> | undefined {
 		return undefined;
 	}
 	return client.stop();
+}
+
+
+// Copy the debugger folder interface DLL from the given subpath within the extension to the given destination
+// folder.
+function copyInterfaceFile(folder: Uri, interfacePath: string) {
+	const dllPath = path.join(extensionBinFolder, interfacePath, "interface.dll");
+	let source = Uri.file(dllPath);
+	let destination = Uri.file(folder.path + "/DebuggerInterface.dll");
+	workspace.fs.copy(source, destination, { overwrite: true }).then(
+		() => {
+			window.showInformationMessage("Unrealscript debugger interface installed");
+		},
+		(reason) => {
+			window.showInformationMessage("Unrealscript debugger interface installation failed: " + reason);
+		}
+	);
+}
+
+// Show an open dialog to pick a destination folder for the debugger interface. Returns a promise for
+// the selected folder.
+function chooseGameFolder() {
+	return window.showOpenDialog({
+		"canSelectFiles": false,
+		"canSelectFolders": true,
+		"canSelectMany": false,
+		"openLabel": "Choose game binary folder"
+	});
+}
+
+// Entry point to copy the debugger interface DLL into a game folder. First prompts the user for
+// 32 or 64 bit, then opens an open dialog box to choose the destination directory. Finally copies
+// the file to that directory.
+function installInterface() {
+	window.showQuickPick(
+		[
+			{ "label": "Install 32-bit debugger interface", "interfacePath": "win32" },
+			{ "label": "Install 64-bit debugger interface", "interfacePath": "win64" }
+		],
+		{ canPickMany: false }
+	).then((pick) => {
+		if (pick)
+			chooseGameFolder().then((folders) => {
+				if (folders)
+					copyInterfaceFile(folders[0], pick.interfacePath);
+			});
+	});
 }
--- gulpfile.js
+++ gulpfile.js
@@ -84,4 +84,94 @@ const PRESETS_TASK = (() => {
     return TASK_NAME;
 })();
 
-gulp.task('default', gulp.series([GRAMMAR_TASK, SYNTAX_TASK, PRESETS_TASK]));
+
+const DEBUGGER_TASK_CLEAR = (() => {
+    const TASK_NAME = 'clear debugger';
+
+    gulp.task(TASK_NAME, (done) => {
+        exec("npx rimraf unrealscript-debugger/target --preserve-root ", (err, stdout, stderr) => {
+            console.log(stdout);
+            console.error(stderr);
+            done();
+        });
+    })
+
+    return TASK_NAME;
+})();
+
+const DEBUGGER_TASK_BUILD = (() => {
+    const TASK_NAME = 'build debugger';
+    const buildCmd = "cd unrealscript-debugger && cargo build --release --target ";
+
+    const build = (targetName, done) => {
+        let hasError = false;
+        const shell = exec(buildCmd + targetName, (err, stdout, stderr) => {
+            done();
+        });
+        shell.stdout.on("data", (data) => {
+            console.log(data);
+        });
+        shell.stderr.on("data", data => {
+            if (hasError) {
+                console.error(data);
+                return;
+            }
+            if (String(data).startsWith("error")) {
+                console.error(data);
+                hasError = true;
+                return;
+            }
+            console.log(data);
+        });
+    }
+    const buildX86 = (done) => {
+        const targetName = "i686-pc-windows-msvc";
+        build(targetName, done);
+    }
+
+    const buildX64 = (done) => {
+        const targetName = "x86_64-pc-windows-msvc";
+        build(targetName, done);
+    }
+
+    gulp.task(TASK_NAME, gulp.series(
+        buildX86,
+        buildX64,
+    ))
+    return TASK_NAME;
+})();
+
+const DEBUGGER_TASK_COPY = (() => {
+    const TASK_NAME = 'copy debugger bin';
+
+    const filesGlobX86 = [
+        'unrealscript-debugger/target/i686-pc-windows-msvc/release/adapter.exe',
+        'unrealscript-debugger/target/i686-pc-windows-msvc/release/interface.dll',
+    ];
+
+    const filesGlobX64 = [
+        'unrealscript-debugger/target/x86_64-pc-windows-msvc/release/adapter.exe',
+        'unrealscript-debugger/target/x86_64-pc-windows-msvc/release/interface.dll',
+    ];
+
+
+    gulp.task(TASK_NAME, gulp.series(
+        () => {
+            return gulp.src(filesGlobX86).pipe(gulp.dest(path.join(__dirname, 'out', 'bin/win32/')))
+        },
+        () => {
+            return gulp.src(filesGlobX64).pipe(gulp.dest(path.join(__dirname, 'out', 'bin/win64/')))
+        }
+    ))
+
+    return TASK_NAME;
+})();
+
+gulp.task('default', gulp.series([
+    GRAMMAR_TASK,
+    SYNTAX_TASK,
+    PRESETS_TASK,
+    // DEBUGGER_TASK_CLEAR, // jump clear to speed up pack
+    DEBUGGER_TASK_BUILD,
+    DEBUGGER_TASK_COPY,
+]));
